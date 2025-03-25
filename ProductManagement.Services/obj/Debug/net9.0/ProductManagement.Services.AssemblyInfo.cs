b89fcaffffc09e79b666e6414279f9723f8a708e
Onur Celebi <onur.celebi@manifesto.co.uk>
2025-03-25 16:13:43 +0000

    Unit test added to the project.

--- ProductManagement.Services/obj/Debug/net9.0/ProductManagement.Services.AssemblyInfo.cs
+++ ProductManagement.Services/obj/Debug/net9.0/ProductManagement.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagement.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d4c03438b3925ee96e8e0d89d7181392fefbb05")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagement.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagement.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
